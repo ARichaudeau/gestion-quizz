e68302973b215dd6111f8522992db82698278401
Amandine <amandine.richaudeau@outlook.fr>
2018-11-14 17:25:58 +0100

    Commit du TP2

--- src/fr/diginamic/model/Question.java
+++ src/fr/diginamic/model/Question.java
@@ -1,17 +1,23 @@
 package fr.diginamic.model;
 
+import java.util.ArrayList;
+
 public class Question {
 	private String intitule;
-	private String[] propositions;
+	private ArrayList<String> propositions;
 	private String bonneReponse;
 	
 	public Question(String intitule, int nb) {
 		super();
 		this.intitule = intitule;
-		this.propositions = new String[nb];
+		this.propositions = new ArrayList<>(nb);
 	}
 	
 	public boolean verifierReponse(String verif) {
 		return verif.equals(bonneReponse);
+	}
+	
+	public void addProposition(String proposition) {
+	propositions.add(proposition);
 	}
 }
